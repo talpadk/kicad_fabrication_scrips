e2188296931b21ee26582cd591380401e478e365
Visti Andresen <visti@how.dk>
2021-03-12 16:38:10 +0100

    Updated the readme generator to support the hfpcb files

--- make_readme.pl
+++ make_readme.pl
@@ -33,26 +33,29 @@ sub flushBuffer(){
 }
 
 
-my %knownTypes = ('Gerber_Pick&Place.txt'=>'PCB-POOL formated pick and place file',
+my %knownTypes = (
+	    'Gerber_Pick&Place.txt'=>'PCB-POOL formated pick and place file',
+			'PnP_for_hfpcb_top_side.txt' => 'Pick and place coordinate and rotation information as per HFPCB supplied example',
 		  'gerbv_pick_and_place.csv'=>'Internal use, pick and place file compatible with the gerbv program',
 		  '-all.pos'=>'Internal use, raw output pick and place data from kicad',
-		  '-B.Cu.gbl'=>'Gerber bottom copper layer',
-		  '-B.Mask.gbs'=>'Gerber bottom solder mask',
-		  '-B.SilkS.gbo'=>'Gerber bottom silk screen',
-		  '-B.Paste.gbp'=>'Gerber bottom solder paste',
+		  '-B.Cu.(gbl|gbr)'=>'Gerber bottom copper layer',
+		  '-B.Mask.(gbs|gbr)'=>'Gerber bottom solder mask',
+		  '-B.SilkS.(gbo|gbr)'=>'Gerber bottom silk screen',
+		  '-B.Paste.(gbp|gbr)'=>'Gerber bottom solder paste',
 		  '-PTH.drl'=>'Drill file for plated holes',
 		  '-NPTH.drl'=>'Drill file for non plated holes',
-		  '-Edge.Cuts.gm1'=>'Gerber edge cuts',
-		  '-F.Cu.gtl'=>'Gerber top copper layer',
-		  '-F.Mask.gts'=>'Gerber top solder mask',
-		  '-F.SilkS.gto'=>'Gerber top silk screen',
-		  '-F.Paste.gtp'=>'Gerber top solder paste',
-		  'board_specification.pdf'=>'A pdf file containing a description of the PCB material to be used',
+		  '-Edge.Cuts.(gm1|gbr)'=>'Gerber edge cuts',
+		  '-F.Cu.(gtl|gbr)'=>'Gerber top copper layer',
+		  '-F.Mask.(gts|gbr)'=>'Gerber top solder mask',
+		  '-F.SilkS.(gto|gbr)'=>'Gerber top silk screen',
+		  '-F.Paste.(gtp|gbr)'=>'Gerber top solder paste',
+			'-job.gbrjob' => 'A Gerber job file detailting the board stack up and other features',
+		  'board_specification.pdf'=>'A pdf file containing a description of the PCB material to be used and other details',
 		  'pcbpool_bom_compact.csv'=>'PCB-POOL formated Bill Of Materials file',
 		  'pcbpool_bom.csv'=>'PCB-POOL formated Bill Of Materials file',
+			'bom.csv' => 'Bill of matterials both for the individual components and grouped by components of the same value and type',
 		  'README.TXT'=>'This file',
 		  'assembly.pdf'=>'A pdf file describing/showing the location and orientation of the different parts'
-		  
     );
 
 
@@ -60,7 +63,6 @@ my %knownTypes = ('Gerber_Pick&Place.txt'=>'PCB-POOL formated pick and place fil
 
 output ("This file describes the content of the folder");
 output ("");
-output ("The gerber files are supposed to have Protel like file suffixes");
 output ("");
 output ("");
 output ("Files:");
